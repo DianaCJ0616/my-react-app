68c28ec5933e4dc5dc99de284b061eaa2d968145
Diana Cardenas-Jara <dianitacj1@gmail.com>
2024-11-12 12:49:30 -0600

    Used fragments to wrap multiple adjacent JSX elements (squares)

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import React, {StrictMode} from "react";
 import { createRoot } from 'react-dom/client';
+import App from "./App";
 import './styles.css';
 
 const root = createRoot(document.getElementById("root"));
